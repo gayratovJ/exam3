a870ff7234bc021e09456d7bf0ad01a1bf710fbb
gayratov <smart17062008@gmail.com>
2023-06-26 14:58:57 +0500

    first commit

---
+++ loading.js
@@ -0,0 +1,5 @@
+const loading = document.getElementById( "loading" );
+
+setTimeout( () => {
+  loading.classList.add( "loading-none" );
+}, 2000 );
---
+++ main.js
@@ -0,0 +1,16 @@
+$( '.owl-theme' ).owltheme( {
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    1000: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+} )
